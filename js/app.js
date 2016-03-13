650952c98d242dab575e43b650714259ff519fb6
Syed M. Taha <s.m.taha10@gmail.com>
2016-03-14 01:26:56 +0500

    Add confirm password field.

--- js/app.js
+++ js/app.js
@@ -56,7 +56,30 @@ myApp.controller('confirmationController', ['$scope', function($scope){
     $scope.officialRollNo = '';
     $scope.signupName = '';
     $scope.signupEmail = '';
-    $scope.signuPassword= '';
+    $scope.signupStudentPassword= '';
+    $scope.signupStudentPasswordCheck = '';
 
-}])
+}]);
+
+myApp.directive('passwordConfirm', function () {
+    return {
+        restrict: 'A',
+        scope: {
+            matchTarget: '=',
+        },
+        require: 'ngModel',
+        link: function (scope, elem, attrs, ctrl) {
+            var validator = function(value){
+                ctrl.$setValidity('match', value === scope.matchTarget);
+                return value;
+            }
+
+            ctrl.$parsers.unshift(validator);
+            ctrl.$formatters.push(validator);
+            scope.$watch('matchTarget', function (newVal, OldVal) {
+                validator(ctrl.viewValue);
+            });
+        }
+    };
+});
 
