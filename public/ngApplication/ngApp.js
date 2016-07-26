10137ca7acbd804cb1944d5119f16218d9bce320
Syed M. Taha <s.m.taha10@gmail.com>
2016-07-26 23:17:29 +0500

    Heading towards angular2

--- public/ngApplication/ngApp.js
+++
@@ -1,9 +0,0 @@
-var app = angular.module('app', []);
-
-app.controller('controller1', ['$scope', '$log',function($scope, $log){
-    $scope.name = 'Saad Abbasi';
-
-    $scope.showname = function(){
-        $log.log('Hello world');
-    }
-} ]);
