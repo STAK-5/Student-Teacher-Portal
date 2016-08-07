09c219d2bcfa6083c0c3446b301d4363c95225f5
Syed M. Taha <s.m.taha10@gmail.com>
2016-08-07 18:23:47 +0500

    Download panel complete

---
+++ assets/app/components/app.download-panel.component.ts
@@ -0,0 +1,13 @@
+/**
+ * Created by syed-muhammad-taha on 8/4/16.
+ */
+
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'stp-download-panel',
+    templateUrl: 'js/app/templates/app.download-panel.template.html'
+})
+export class StpDownloadPanel{
+
+}
