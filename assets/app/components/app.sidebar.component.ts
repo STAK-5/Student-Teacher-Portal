ce2f31452947d1f170f06153f72ea5659ab29689
Syed M. Taha <s.m.taha10@gmail.com>
2016-08-07 18:22:46 +0500

    Sidebar complete

---
+++ assets/app/components/app.sidebar.component.ts
@@ -0,0 +1,13 @@
+/**
+ * Created by syed-muhammad-taha on 8/4/16.
+ */
+
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'stp-side-bar',
+    templateUrl: 'js/app/templates/app.sidebar.template.html'
+})
+export class StpSideBar {
+
+}
